1f571f3b7e2038f4ec7c6d02ac671813df5d98e8
tusharthakur003 <tusharthakur3136@gmail.com>
2025-03-17 00:52:02 +0530

    [Tushar Kumar] Add. UC10

--- HelloGreetingApplication/Program.cs
+++ HelloGreetingApplication/Program.cs
@@ -21,8 +21,8 @@ try
 
     builder.Services.AddScoped<IGreetingBL, GreetingBL>();
     builder.Services.AddScoped<IGreetingRL, GreetingRL>();
-    //builder.Services.AddScoped<IUserBL, UserBL>();
-    //builder.Services.AddScoped<IUserRL, UserRL>();
+    builder.Services.AddScoped<IUserBL, UserBL>();
+    builder.Services.AddScoped<IUserRL, UserRL>();
 
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
